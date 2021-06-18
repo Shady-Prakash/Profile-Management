af3b4f7c10634e87c3ab12b339d40b366e4e160c
Prakash Mahat <shadyprakash8@gmail.com>
2021-06-17 22:05:03 -0700

    comment add and delete api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -325,6 +325,7 @@ router.get('/github/:username', (req, res) => {
 			res.json(JSON.parse(body));
 		});
 	} catch (err) {
+		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
 });
