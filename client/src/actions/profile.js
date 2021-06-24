1bccb683820ad7830b3e2168d6a58ebc31cbda2f
Prakash Mahat <shadyprakash8@gmail.com>
2021-06-24 06:49:34 -0700

    finish profile actions & reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -9,6 +9,8 @@ import {
 	UPDATE_PROFILE,
 	CLEAR_PROFILE,
 	ACCOUNT_DELETED,
+	GET_PROFILES,
+	GET_REPOS,
 } from './types';
 
 // Get current users profile
@@ -18,6 +20,63 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {msg: err.response.statusText, status: err.response.status},
+		});
+	}
+};
+
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+	dispatch({type: CLEAR_PROFILE});
+
+	try {
+		const res = await axios.get('/api/profile');
+
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {msg: err.response.statusText, status: err.response.status},
+		});
+	}
+};
+
+// Get profile by ID
+export const getProfile = (userId) => async (dispatch) => {
+	dispatch({type: CLEAR_PROFILE});
+
+	try {
+		const res = await axios.get(`/api/profile/${userId}`);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {msg: err.response.statusText, status: err.response.status},
+		});
+	}
+};
+
+// Get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+	dispatch({type: CLEAR_PROFILE});
+
+	try {
+		const res = await axios.get(`/api/profile/github/${username}`);
+
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data,
 		});
 	} catch (err) {
